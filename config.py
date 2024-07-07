fd796c18956f5dc58677ad413cb6156b693d9a3c
Muhammad Faiz <muhammadfaiz7130@gmail.com>
2024-07-08 01:09:10 +0700

    fix logo

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = 'sJpufMj1em'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost:5432/bansosdb'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@103.27.206.29:5432/bansosdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
